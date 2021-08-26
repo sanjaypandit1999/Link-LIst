786c0a9037587fee7e1d742fd5c69cd89c6958ce
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-26 23:58:45 +0530

    ADDED:Insert Node

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -10,7 +10,7 @@ public class LinkedList {
         list.append(56);
         list.append(70);
         list.append(30);
-        list.searh(30);
+        list.insertAt(2, 40);
         list.display();
     }
 }
